a70e10cf5681d0ca56e83ce1c8810a0dc43e5f73
Fábio Barboza Coelho de Souza <fabiobarboza7@hotmail.com>
2019-06-30 22:59:18 -0300

    adding production stuff in database.rb

--- config/database.rb
+++ config/database.rb
@@ -15,3 +15,16 @@ configure :development do
     pool: 5
   }
 end
+
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///postgres/onebitbot_production')
+ 
+  set :database, {
+    adapter:  'postgresql',
+    host:     db.host,
+    username: db.user,
+    password: db.password,
+    database: db.path[1..-1],
+    encoding: 'utf8'
+  }
+end
